107778a00dd1d3772426b0b47758eac342e5f626
skececi <sam.kececi@gmail.com>
2021-10-10 17:38:45 -0400

    styling + state

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,50 +1,12 @@
 import * as React from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import * as Haptics from 'expo-haptics';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { useEffect } from "react";
-
-// TODO- make everything enums (+ custom)
-export type Task = {
-  taskTitle: string;
-  taskCategory: string;
-  completedToday: boolean;
-}
-
-export default function MainScreen() {
-  const [taskList, setTaskList] = React.useState([]);
-  // TODO- load state from some storage (look into local storage- account based)
-
-  const meditationTask: Task = {
-    taskTitle: "Meditate 5 minutes",
-    taskCategory: "Mental Health",
-    completedToday: false, // TODO state-ify this?
-  };
-
-  const dailyTasks: Task[] = [meditationTask];
-  const tasksButtons = dailyTasks.map((task, index) => {
-    return (
-      <TouchableOpacity
-        key={index}
-        onPress={() => {
-          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
-        }}>
-        <Text> {task.taskTitle} </Text>
-      </TouchableOpacity>
-    )
-  });
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Main - buttons to confirm etc etc</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
-      {tasksButtons}
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -62,3 +24,87 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+// TODO- make everything enums (+ custom string option)
+export type Task = {
+  taskTitle: string;
+  taskCategory: string;
+  lastCompleted?: Date;
+};
+
+// Map of Date -> Task (completed)
+// Map of Date -> Journal entry
+// Map of Date -> Num tasks?
+
+const meditationTask: Task = {
+  taskTitle: "Meditate 5 minutes",
+  taskCategory: "Mental Health",
+};
+
+const exerciseTask: Task = {
+  taskTitle: "Exercise 15 minutes",
+  taskCategory: "Physical Health",
+};
+
+export default function MainScreen() {
+  const [taskList, setTaskList] = React.useState([]);
+  // TODO- load state from some storage (look into local storage- account based)
+  // TODO- handling of completion
+  async function storeTaskList(taskList: Task[]) {
+    try {
+      await AsyncStorage.setItem('@task_list', JSON.stringify(taskList));
+    } catch (e) {
+      // saving error
+      console.error("saving error");
+    }
+  }
+
+  // TODO- have the date cutoff be dynamic and flexible
+  function setCompleted(task: Task) {
+    task.lastCompleted = new Date();
+  }
+
+  useEffect(() => {
+    storeTaskList(taskList);
+  }, [taskList]);
+
+
+  const dailyTasks: Task[] = [meditationTask, exerciseTask];
+  const tasksButtons = dailyTasks.map((task, index) => {
+    return (
+      <TouchableOpacity
+        style={{
+          elevation: 8,
+          backgroundColor: "#A2F9DE",
+          borderRadius: 10,
+          paddingVertical: 10,
+          paddingHorizontal: 12
+        }}
+        key={index}
+        onPress={() => {
+          setCompleted(task);
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        }}>
+        <Text style={{
+          fontSize: 18,
+          color: "black",
+          fontWeight: "bold",
+          alignSelf: "center",
+          textTransform: "uppercase"
+        }}>
+          {task.taskTitle}
+        </Text>
+      </TouchableOpacity>
+    )
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Main - buttons to confirm etc etc</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+      {tasksButtons}
+    </View>
+  );
+}
+
+
